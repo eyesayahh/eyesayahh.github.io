236c3cbf21a49a07e070fda3ae3447f9a5b80092
Zay Macbook <isaiahaquino1997@gmail.com>
2022-07-13 19:27:09 -0700

    fix mobile layout

---
+++ script.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('.mainTitle');
+
+title.classList.add('appear');
+
+const techImgs = document.querySelectorAll('.techImg');
+techImgs.forEach(img => {
+  img.addEventListener('click', () => {
+    img.classList.toggle('clickSpin');
+    setTimeout(() => {
+      img.classList.toggle('clickSpin');
+    }, 1000);
+  });
+});
